3d27c8fd9597f710c25697c374dc857934b206b1
gilbabytech <gilbabytech@outlook.com>
2026-03-16 12:52:22 -0400

    script update

--- DeployObjectTest.ps1
+++ DeployObjectTest.ps1
@@ -1,1 +1,12 @@
-New-AzResourceGroupDeployment -Name seeoutput -ResourcesGroupName rg-lab-prd-web01 -TemplateFile \C:\Users\gilba\github\ARM_Templates\DeployPbjectTest.json
+# This script is used to test function and output them before real deployment
+# Prompt the user for Deployment Name (give specific info to track)
+# Use this for the RG = rg-lab-prd-web01
+
+
+$AzDplyName = Read-Host "Enter ARM Template Deployment name (no spaces)"
+$templateFile = "C:\Users\gilba\github\ARM_Templates\DeployObjectTest.json" 
+
+New-AzResourceGroupDeployment `
+    -name $AzDplyName `
+    -TemplateFile $templateFile `
+    -ResourceGroupName "rg-lab-prd-web01" `
