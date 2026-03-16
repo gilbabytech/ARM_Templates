e227dbc4f406067912d38f780b2861be5571d5b3
gilbabytech <gilbabytech@outlook.com>
2026-03-16 19:45:31 -0400

    file update

--- DeployObjectTest.ps1
+++ DeployObjectTest.ps1
@@ -2,11 +2,14 @@
 # Prompt the user for Deployment Name (give specific info to track)
 # Use this for the RG = rg-lab-prd-web01
 
+#Get-AzResourceGroup | Where-Object { $_.resourcegroupName -like "*rg-*" } | select-object resourcegroupname
+cmd /c 'pause'
 
 $AzDplyName = Read-Host "Enter ARM Template Deployment name (no spaces)"
 $templateFile = "C:\Users\gilba\github\ARM_Templates\DeployObjectTest.json" 
 
 New-AzResourceGroupDeployment `
     -name $AzDplyName `
-    -TemplateFile $templateFile `
+
+-TemplateFile $templateFile `
     -ResourceGroupName "rg-lab-prd-web01" `
